729aed50847325230b3a408e18b24a8caee39141
Fatkhan Masruri <fatkhan.masruri45@gmail.com>
2020-08-31 12:51:04 +0700

    kurang gabungis javascript ke html

---
+++ index.js
@@ -0,0 +1,17 @@
+function changeContent(){
+    //content-nama
+    document.getElementById('contentnama').innerHTML = "Fatkhan Masruri";
+    document.getElementById('namaicon').src = 'icon/iconpersonal.png';
+    //content-lahir
+    document.getElementById('lahircontent').innerHTML = "Kebumen, 13 Juni 2000";
+    document.getElementById('lahiricon').src = 'icon/iconbirth.png';
+    //content-alamat
+    document.getElementById('contentalamat').innerHTML = "Bandung, Jawa Barat, Indonesia";
+    document.getElementById('alamaticon').src = 'icon/iconlocation.png';
+    //content-penedidikamn
+    document.getElementById('contentpendidikan').innerHTML = "Institut Teknologi Bandung";
+    document.getElementById('pendidikanicon').src = "icon/iconpendidikan.png";
+    //content-moto
+    document.getElementById('contentmotivasi').innerHTML="Mlaku maju mandeng ngarep";
+    document.getElementById('motoicon').src = 'png/iconmotivasion.png';
+}
